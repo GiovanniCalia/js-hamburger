fde69a12a677c416bc7b554d0955f3e5ea62dba0
GiovanniCalia <johnca@outlook.it>
2022-02-22 19:51:26 +0100

    Tentativo creazione esercizio

---
+++ main.js
@@ -0,0 +1,9 @@
+let hamburgerAppear = document.querySelector("hamburger-menu");
+console.log(hamburgerAppear);
+
+
+
+/*hamburgerAppear.addEventListener('click', function() {
+    list.display = block;
+})*/
+ 
